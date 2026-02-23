a7b05664222df53c269d18c44846718ab67b767f
camcam_mlv <camille.mallevay@free.fr>
2026-02-23 16:04:59 +0100

    feat(models): ajouter les entités Intervenant, Cours

---
+++ src/main/domain/cours.py
@@ -0,0 +1,6 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+
+class Cours(BaseModel):
+    id: Optional[int] = Field(default=None)
+    libelle: str = Field(..., min_length=2, description="Intitulé du cours (ex: Programmation Web)")
